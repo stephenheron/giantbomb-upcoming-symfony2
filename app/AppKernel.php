b0893d7d411739c4ba93e3419704afeae173e1a8
Stephen Heron <stephen.heron@gmail.com>
2015-08-10 17:01:51 +0100

    FOSUserBundle + SonataUserBundle now in place.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,6 +23,8 @@ class AppKernel extends Kernel
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
             new Sonata\AdminBundle\SonataAdminBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new Sonata\UserBundle\SonataUserBundle('FOSUserBundle'),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
